8219563a7da25caaa0f4225eadf47bc691f09225
Abhisek Pan <pana@purdue.edu>
2013-04-27 14:33:01 -0400

    Modifying best_partition.py to consider hybrid reuse distances

--- cp_utilities/best_partition.py
+++ cp_utilities/best_partition.py
@@ -6,6 +6,7 @@ NAME
 
 SYNOPSYS
     ./best_partition.py benchmark input_file num_threads set_bits total_ways
+    is hybrid
 
 DESCRIPTION
     Given the reuse-distance signatures of each thread for each interval for
@@ -14,12 +15,23 @@ DESCRIPTION
     algorithm starts by dividing the total allocation equally among all
     threads. Then the allocation of the preferred thread is increased by (n-1)
     where n = number of threads. The allocation for all other threads is
-    reduced by 1. The algorith computes the gain for this allocation, the gain
+    reduced by 1. The algorithm computes the gain for this allocation, the gain
     being equal to the references included by increasing the allocation in the
     preferred thread minus the references left out by reducing the allocaion
     in all other threads. This step is repeated till the allocation for the
     other threads reduces to 1. The allocation which shows the maximum gain
     is chosen as the best allocation for a particular preferred thread.
+    
+    For hybrid scheme, the algorithm considers both shared and private reuse
+    distance profiles. According to our observations, shared blocks are better
+    utilized when they are allocated in the preferred partition, since these
+    blocks can stay in this partition for longer time. So in hybrid sheme, we
+    consider the shared reuse distance profile and the references included in
+    the shared profile, starting from dist 0, are also included in the gain in
+    allocating ways to the preferred thread. So for example, when 3 extra ways
+    are allocated to the preferred thread beyond its normal allocation, the
+    references with distance less than 3 in the shared profile are also
+    considered in the gain obtained.
  
 OPTIONS
     benchmark
@@ -38,10 +50,14 @@ OPTIONS
     total_ways
         Total number of ways for all threads. Should be divisible by number of
         threads.
+    
+    is_hybrid
+        Are we processing hybrid reuse distance. That would require 2 stacks.
+        0 = no, 1 = yes.
                        
 EXAMPLES
     ./best_partition.py  blackscholes inter_rda_blackscholes_large_4_5mil.out 4
-    9 32
+    9 32 0
 
 NOTES
 
@@ -68,7 +84,7 @@ def best_partition():
     #=======================================================================
     # command line processing
     #=======================================================================
-    if len(sys.argv) != 6:
+    if len(sys.argv) != 7:
         sys.stdout.write("Incorrect number of arguments. Program description:\n" 
                          + __doc__)
         sys.exit(1)
@@ -77,10 +93,27 @@ def best_partition():
     num_threads = int(sys.argv[3])
     num_sets = 2 ** int(sys.argv[4])
     num_ways = int(sys.argv[5])
-    new_bm = bm.Benchmark(benchmark, num_threads, num_sets, num_ways)
-    new_bm.read_rddata_from_file(input_file)
-    new_bm.build_freq_vs_capacity_profile()
-    _ = new_bm.find_best_partition()
+    is_hybrid = int(sys.argv[6])
+    if not(is_hybrid):
+        stack_type = None
+        new_bm = bm.Benchmark(benchmark, num_threads, stack_type,
+                              num_sets, num_ways)
+        new_bm.read_rddata_from_file(input_file, num_threads)
+        new_bm.build_freq_vs_capacity_profile()
+        _ = new_bm.find_best_partition()
+    else:
+        stack_type = 'private'
+        new_bm_p = bm.Benchmark(benchmark, num_threads, stack_type,
+                                num_sets, num_ways)
+        new_bm_p.read_rddata_from_file(input_file, num_threads)
+        new_bm_p.build_freq_vs_capacity_profile()
+        
+        stack_type = 'shared'
+        new_bm_s = bm.Benchmark(benchmark, num_threads, stack_type,
+                              num_sets, num_ways)
+        new_bm_s.read_rddata_from_file(input_file, num_threads)
+        new_bm_s.build_freq_vs_capacity_profile()
+        _ = new_bm_p.find_best_partition(shared_profile=new_bm_s)
     sys.stderr.write("my work is done here\n")
 
 
