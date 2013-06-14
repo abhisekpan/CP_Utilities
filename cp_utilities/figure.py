51c3e4a4735d0e2810300122acf03274675adfb0
Abhisek Pan <pana@purdue.edu>
2013-06-14 11:21:32 -0400

    clean up figure.py - separate axes legend and spine formatting from plot data

--- cp_utilities/figure.py
+++ cp_utilities/figure.py
@@ -14,7 +14,7 @@ import sys
 
 
 _inches_per_pt = 1.0 / 72.27               # Convert pt to inch
-_golden_mean = (math.sqrt(5) - 1.0) / 2.0  # Aesthetic ratio
+_golden_mean = 2.0 / (math.sqrt(5) + 1.0)  # 1 / Aesthetic ratio
 _lines = ['-', '--', '-.', ':']
 _markers = ['3', 'x', '2', '+']
 _linecycler = cycle(_lines)
@@ -23,28 +23,29 @@ _patterns = ('//', '*', 'o', '\\', 'O', '.')
 
 
 class Figure(object):
-    
-    """ Handles the plotting environment and the actual plotting for a figure. 
+    """
+    Handles the plotting environment and the actual plotting for a figure. 
     
     This class acts as a wrapper around the matplotlib. Matplotlib provides
     too many options! This class restricts those choices and makes it a little 
-    easier for me to handle. The figure will be plotted in one file. 
+    easier for me to handle. The figure will be plotted in one file. Output
+    format is PDF.
     There can be multiple sub-plots in the figure. When all plots are added, 
     the figure should be saved and closed.
     """    
     
-    def __init__(self, filename, figformat='pdf', title=None, columns=1,
-                 total_subplots=1, subplots_per_page=1, fig_width_pt=175.0,
+    def __init__(self, filename, title=None, columns=1,
+                 total_subplots=1, subplots_per_page=1, column_width_pt=175.0,
                  font_size=6, title_font_size=6):
-        """Initialize the plotting environment. 
+        """
+        Initialize the plotting environment. 
         
         @param filename: file name where figure will be plotted
-        @param figformat: format of figure, pdf / eps currently
         @param title: Title of the figure
         @param columns: number of columns
         @param total_subplots: total number of sub-plots in this figure
-        @param subplots_per_page: subplots in 1 page, only for pdf format
-        @param fig_width_pt: width of one column in pixels, 
+        @param subplots_per_page: subplots in 1 page
+        @param column_width_pt: width of one column in pixels, 
         @param font_size: font size for everything except title
         @param title_font_size: font size for title
         
@@ -55,36 +56,26 @@ class Figure(object):
         
         Get the column width in pixels from LaTeX using \showthe\columnwidth.
 
-        For pdf format, we can create a multi-page file, hence we can control
-        the number of subplots per page.
+        We can create a multi-page file, hence we can control the number of 
+        subplots per page.
         
         """
-        if figformat not in ('pdf', 'eps'):
-            sys.stderr.write("Invalid figure format. Aborting...\n")
-            sys.exit(1)
-        if figformat == 'pdf':
-            self.figformat = 'PDF'
-            self.filename = PdfPages(filename + '.' + figformat)
-            self.subplots_per_page = subplots_per_page
-        else:
-            self.figformat = 'PS'
-            self.filename = filename + '.' + figformat
-            self.subplots_per_page = total_subplots
+        self.figformat = 'PDF'
+        self.filename = PdfPages(filename + '.' + figformat)
+        self.subplots_per_page = subplots_per_page
         self.total_subplots = total_subplots
-        self.num_pages = int(math.ceil(self.total_subplots /
-                                         float(self.subplots_per_page)))
+        self.current_plot = self.current_page = 0
+        #self.num_pages = int(math.ceil(self.total_subplots /
+        #    float(self.subplots_per_page)))
         self.columns = columns
-        self.rows = int(math.ceil(self.subplots_per_page / 
-                                        self.columns))
-        self.current_plot = 0
-        fig_width_in = fig_width_pt * _inches_per_pt * self.columns # width in inches
-        fig_height_in = fig_width_in * _golden_mean * self.rows # height in inches
+        self.rows = int(math.ceil(self.subplots_per_page / self.columns))
+        fig_width_in = column_width_pt * _inches_per_pt * self.columns 
+        fig_height_in = fig_width_in * _golden_mean * self.rows 
         fig_size = [fig_width_in, fig_height_in]
         self.plot_params = {'backend': self.figformat,
                'font.size': font_size,
                'axes.labelsize': font_size,
-               'text.fontsize': font_size,
-               'legend.fontsize': font_size - 1, # - 2,
+               'legend.fontsize': font_size,
                'legend.labelspacing': 0.05,
                'xtick.labelsize': font_size,
                'ytick.labelsize': font_size,
@@ -93,112 +84,207 @@ class Figure(object):
         pl.rcParams.update(self.plot_params)
         self.title = title
         self.title_font_size = title_font_size
-        self.create_new_figure()
 
-    def create_new_figure(self):
-        """Create a new figure"""
+    def create_new_figure(self, print_title=False):
+        """
+        Create a new figure.
+        
+        Title is printed only for the first page. Hence the flag.
+
+        """
         self.figure = pl.figure()
-        if self.title is not None:
+        if (print_title == True) and (self.title is not None):
             self.figure.suptitle(self.title, fontsize=self.title_font_size)
 
-    def set_plot_param(self, subplot, xlabel=None, ylabel=None, title=None, xticks=None,
-                        rect=[0.1, 0.15, 0.8, 0.7], legend=True, legend_loc=1):
-        """Add a new axis with parameters and return the axis.
-        @param subplot: the subplot for which the axis is set 
-        @param xlabel: label for x axis
-        @param ylabel: label for y axis
-        @title title of the figure using this axis
-        @param rect: space between the axes and the end of fig  -
-        [left, bottom, width, height] in normalized (0, 1) units.
-        @param legend: control printing of legends. 
-        @param legend_loc: location of the legend. defaults is on the right 
-        
+    def handle_page_break(self):
+        """
+        Handle page breaks based on the number of subplots added.
+        Save the figure at the end of each page. Create a new figure
+        at the start of each page.
+        Return plot id in page.
+
+        """
+        assert self.current_plot < self.total_subplots, \
+        "too many sub-plots in this figure!"
+        plot_id_in_page = (self.current_plot % self.subplots_per_page) + 1
+        new_page = (plot_id_in_page == 1)
+        if new_page:
+            if self.current_page == 0:
+                self.create_new_figure(print_title=True)
+            else:
+                self.figure.savefig(self.filename, format=self.figformat,
+                                    bbox_inches='tight')
+                self.create_new_figure()
+            self.current_page += 1
+        self.current_plot += 1
+        return plot_id_in_page
+
+    def set_legend_format(self, sp, legend_loc=0, legend_ncol=1,
+                          bbox_to_anchor=None, **kwargs):
+        """
+        Format legend for a subplot.
+
+        @param sp: the subplot for which legends are set 
+        @param legend_loc: location of the legend.
+        @param legend_ncol: number of columns in the legend.
+        @param bbox_to_anchor: specfy legend loc in normalized co-ordinates
+        @param **kwargs: any other relevant parameters
+
         The location codes for legend are
         Location String 	Location Code
-        'best'    0
+        'best'          0
     	'upper right'	1
     	'upper left'	2
     	'lower left'	3
     	'lower right'	4
-    	'right'	5
+    	'right'  	5
     	'center left'	6
     	'center right'	7
     	'lower center'	8
     	'upper center'	9
     	'center'	10
 
-        """
-        #subplot.set_position(rect)
-        subplot.ticklabel_format(style='sci',scilimits=(-3,4),axis='both')
-        #subplot.get_yaxis().get_major_formatter().set_scientific(True)
-        #subplot.xaxis.set_major_locator( MultipleLocator(1.00) )
-        #subplot.yaxis.set_major_locator( AutoLocator )
-        if xlabel:  subplot.set_xlabel(r'\textit{' + xlabel + '}')
-        if ylabel:  subplot.set_ylabel(r'\textit{' + ylabel + '}')
-        if title is not None: subplot.set_title(title.encode('string-escape'))
-        if legend:  subplot.legend(loc=9, bbox_to_anchor=(0.65, 0.9), ncol=2)
-        #if legend:  subplot.legend(loc=legend_loc)
-        #if legend:  subplot.legend(loc=5)
-        if xticks:  subplot.set_xticklabels(xticks, rotation=70)
-        #subplot.locator_params(tight=True)
-
-    def add_plot(self, new_style=False, labels=None, xlabel=None, ylabel=None,
-                title=None, xticks=None, *args, **kwargs):
-        """Add a new sub-plot to the figure.
+        bbox_to_anchor is used to specify any arbitrary location for the legend.
+        For example,
+        loc = 'upper right', bbox_to_anchor = (0.5, 0.5)
+        will place the legend so that the upper right corner of the legend at
+        the center of the axes.
         
-        If pdf format is used savefig needs to be called for each new page.
-        For eps format, savefig is called only once after all subplots are
-        added.
+        """
+        sp.legend(loc=legend_loc, ncol=legend_ncol,
+                       bbox_to_anchor=bbox_to_anchor, **kwargs)
+    
+    def set_axis_format(self, sp, axis, label=None, ticks=None, limits=None, 
+                        tick_labels=None, scientific=False):
+        """
+        Set parameters for an axis.
+
+        @param sp: the subplot for which parameters are set 
+        @param axis: the axis to format: x, y
+        @param label: label for axis
+        @param: ticks: list of ticks
+        @param: limits: data limits for axis (min, max)
+        @param: tick_labels: labels for individual ticks
+        @param: scientific: scientific notation for tick labels
+       
+        Notes about axis modifiers:
+        Formatter:
+        Formatter determines how ticks locations are converted into strings.
+        Interesting formatters:
+        FixedFormatter: sets the strings manually for the lables
+        FuncFormatter: user-defined functions set the labels
+        FormatStrFormatter: use a spritf format string
+        ScalarFormatter: tick is a plain old number, default for scalars
+        LogFormatter: Formatter for log axes
+        
+        ticklabel_format is a convenience wrapper function that sets the ticks
+        in scientific format by calling the methods of the axes Formatter.
+        Works only with ScalarFormatter (defualt). For example,
+        subplot.ticklabel_format(style='sci',scilimits=(-3,4),axis='both')
+        can be replaced by the following for each of the axes:
+        subplot.get_xaxis().get_major_formatter().set_scientific(True)
+        subplot.get_xaxis().get_major_formatter().set_powerlimits((-3,4))
+
+        set_xticklabels is a convinience wrapper function that uses the 
+        FixedFormatter to set the labels of xticks with the labels passed.
+
+        Locator:
+        Locator handles auto-scaling of tick limits based on data limits and
+        choosing of tick locations. Interesting locators:
+        FixedLocator: Tick Locations are fixed
+        IndexLocator: Locator for index plots (eg. where x = range(len(y)))
+        LinearLocator: Evenly spaced ticks from min to max
+        LogLocator: Logarithmically ticks from min to max
+        MultipleLocator: Ticks and range on multiples of bases
+        AutoLocator: A maximum number of ticks at nice locations
+        For example:
+        subplot.xaxis.set_major_locator( MultipleLocator(1.00) )
+        subplot.yaxis.set_major_locator( AutoLocator )
+
+        set_ticks_position determines if the ticks will be visibile on both
+        sides of data (bottom and top axes for x-axis) or just one side.
+    
+        Additional commands that might be helpful:
+        set the space between the axes and the end of fig  -
+        [left, bottom, width, height] in normalized (0, 1) units.
+        subplot.set_position([0.1, 0.15, 0.8, 0.7]) 
 
         """
-        self.current_plot += 1
-        assert self.current_plot <= self.total_subplots, \
-        "too many sub-plots in this figure!"
-        if new_style == True: 
-            kwargs['linestyle'] = next(_linecycler)
+        assert axis in ['x', 'y'], "axis has to be x or y"
+        if axis == 'x':
+            sp.xaxis.set_ticks_position('bottom')
+            if label: sp.set_xlabel(r'\textit{' + label + '}')
+            if ticks: sp.set_xticks(ticks)
+            if limits: sp.set_xlim([0,1])
+            if tick_labels: sp.set_xticklabels(tick_labels, rotation=70)
+            if scientific:
+                subplot.ticklabel_format(style='sci',scilimits=(-3,4),axis='x')
         else:
-            kwargs['linestyle'] = '' #_lines[0]      # solid line by default
+            sp.yaxis.set_ticks_position('left')
+            if label: sp.set_ylabel(r'\textit{' + label + '}')
+            if ticks: sp.set_yticks(ticks)
+            if limits: sp.set_ylim([0,1])
+            if tick_labels: sp.set_yticklabels(tick_labels, rotation=70)
+            if scientific:
+                subplot.ticklabel_format(style='sci',scilimits=(-3,4),axis='y')
+    
+    def set_subplot_title(self, sp, title):
+        """
+        Set subplot title.
+
+        @param sp: the subplot for which parameters are set 
+        @param title: title of the subfigure using this axis
+
+        """
+        sp.set_title(title.encode('string-escape'))
+    
+    def set_spine_format(self, sp):
+        """
+        Set the format of the spines, which are the axes lines that join
+        the ticks.
+
+        They can be positioned with respect to the axes or data limits
+        (used here). Smart Bounds - Spines  attempt to set bounds in a 
+        sophisticated way.
+        """
+        for direction in ['left','bottom']:
+            sp.spines[direction].set_position(('data', -1.0))
+            sp.spines[direction].set_smart_bounds(True)
+        for direction in ['right','top']:
+            sp.spines[direction].set_color('none')
+
+    def add_plot(self, line_plot=False, new_style=False, labels=None,
+                 xlabel=None, ylabel=None, title=None, xtick_labels=None,
+                 legend=True, *args, **kwargs):
+        """
+        Add a new sub-plot to the figure.
+
+        """
+        if line_plot == True:
+            if new_style == True: kwargs['linestyle'] = next(_linecycler)
+            else: kwargs['linestyle'] = lines[0]  # Solid line by default.
+        else:
+                kwargs['linestyle'] = '' # No lines, only points.
         kwargs['markersize'] = 4.0
-        #kwargs['color'] = 'k'
-        plot_id_in_page = self.current_plot % self.subplots_per_page
-        if plot_id_in_page == 0: plot_id_in_page = self.subplots_per_page
+        plot_id_in_page = self.handle_page_break()
         sp = self.figure.add_subplot(self.rows, self.columns, plot_id_in_page)
-        sp.set_ylim([0,1])
-        #for direction in ["left"]:
-            #sp.spines[direction].set_position(('data', -1.0))
-            #sp.spines[direction].set_smart_bounds(True)
-        #for direction in ["right","top"]:
-            #sp.spines[direction].set_color('none')
-        #sp.xaxis.set_ticks_position('bottom')
-        #sp.yaxis.set_ticks_position('left')
         lines = sp.plot(*args, **kwargs)
-        #sp.set_yscale('log')
-        sp.set_xticks(args[0])
         if labels:
             dummy = [line.set_label(label) for line, label in zip(lines, labels)]
         dummy = [line.set_marker(marker) for line, marker in zip(lines, _markers)]
-        self.set_plot_param(sp, xlabel=xlabel, ylabel=ylabel, title=title, xticks=xticks, legend=True)
-        new_page = ((self.current_plot % self.subplots_per_page) == 0)
-        last_page = (self.current_plot == self.total_subplots)
-        if new_page or last_page :
-            self.figure.savefig(self.filename, format=self.figformat, bbox_inches='tight')
-            self.num_pages = self.num_pages - 1
-            if (self.num_pages > 0):
-                self.create_new_figure()
+        self.set_axis_format(sp, 'x', label=xlabel, ticks=args[0],
+                             tick_labels=xtick_labels)
+        self.set_axis_format(sp, 'y', label=ylabel, limits=[0,1])
+        if title: self.set_subplot_title(title)
+        if legend: self.set_legend_format(sp)
         return sp
     
     def add_stackedbar(self, labels=None, xlabel=None, ylabel=None,
-                title=None, xticks=None, *args, **kwargs):
-        """Add a new stacked bar sub-plot to the figure.
-        
-        If pdf format is used savefig needs to be called for each new page.
-        For eps format, savefig is called only once after all subplots are
-        added.
+                title=None, xtick_labels=None, legend=True, *args, **kwargs):
+        """
+        Add a new stacked bar sub-plot to the figure.
 
         """
-        self.current_plot += 1
-        assert self.current_plot <= self.total_subplots, \
-        "too many sub-plots in this figure!"
         kwargs['align'] = 'center'
         kwargs['linewidth'] = 0.1
         kwargs['width'] = 0.35
@@ -206,43 +292,26 @@ class Figure(object):
         patterncycler = cycle(_patterns)
         ind_axis = args[0]
         num_stacks = len(args) - 1
-        plot_id_in_page = self.current_plot % self.subplots_per_page
-        if plot_id_in_page == 0: plot_id_in_page = self.subplots_per_page
+        plot_id_in_page = self.handle_page_break()
         sp = self.figure.add_subplot(self.rows, self.columns, plot_id_in_page)
         for i in xrange(1, num_stacks + 1):
             lower_stacks = np.sum([args[j] for j in xrange(1,i)], axis=0)
             kwargs['color'] = next(colorcycler)
-            #kwargs['alpha'] = 1
             kwargs['edgecolor'] = 'black'
-            #kwargs['hatch'] = next(patterncycler)
-            #kwargs['bottom'] = lower_stacks
             rectangles = sp.bar(ind_axis, args[i], bottom=lower_stacks, **kwargs)
             if labels: rectangles[0].set_label(labels[i-1])
-        for direction in ["left","bottom"]:
-            sp.spines[direction].set_position(('data', -1.0))
-            sp.spines[direction].set_smart_bounds(True)
-            for direction in ["right","top"]:
-                sp.spines[direction].set_color('none')
-            sp.xaxis.set_ticks_position('bottom')
-            sp.yaxis.set_ticks_position('left')
-            #sp.grid(True)
-        sp.set_xticks(args[0])
-        self.set_plot_param(sp, xlabel=xlabel, ylabel=ylabel, title=title, xticks=xticks, legend=True)
-        new_page = ((self.current_plot % self.subplots_per_page) == 0)
-        last_page = (self.current_plot == self.total_subplots)
-        if new_page or last_page :
-            self.figure.savefig(self.filename, format=self.figformat, bbox_inches='tight')
-            self.num_pages = self.num_pages - 1
-            if (self.num_pages > 0):
-                self.create_new_figure()
+        self.set_spine_format(sp)
+        self.set_axis_format(sp, 'x', label=xlabel, ticks=args[0],
+                             tick_labels=xtick_labels)
+        self.set_axis_format(sp, 'y', label=ylabel)
+        if title: self.set_subplot_title(title)
+        if legend: self.set_legend_format(sp)
         return sp
     
     def save_and_close(self):
         """Save & close the figure and free resources."""
-        if self.figformat == 'PDF':
-            self.filename.close()
+        self.figure.savefig(self.filename, format=self.figformat, 
+                            bbox_inches='tight')
+        self.filename.close()
         pl.close(self.figure)
          
-#pl.tight_layout(pad=1.2)
-#pl.subplots_adjust(bottom=0.1, top=0.9, left=1.0, right=1.1)
-
--- plot_examples/plot_stackedbar.py
+++ plot_examples/plot_stackedbar.py
@@ -2,6 +2,7 @@
 # a stacked bar plot
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 def plot_bar(width, *args):
     num_stacks = len(args) - 1
@@ -9,24 +10,40 @@ def plot_bar(width, *args):
     colors = ['r','y','b']
     labels = ['Men', 'Women', 'Dogs']
     ret_values = [None] * num_stacks
+    op_file = PdfPages('stackedbar.pdf')
+    fig = plt.figure()
+    kwargs=dict()
+    kwargs['linewidth'] = 0.1
+    sp = fig.add_subplot(1, 1, 1)
     for i in xrange(1, num_stacks + 1):
         lower_stacks = np.sum([args[j] for j in xrange(1,i)], axis=0)
         print lower_stacks
         #ret_values[i - 1] 
-        p = plt.bar(ind, args[i], width, color=colors[i-1], bottom=lower_stacks)
+        p = sp.bar(ind, args[i], width, color=colors[i-1], bottom=lower_stacks, **kwargs)
         print p
         p[0].set_label(labels[i-1])
         #p2 = plt.bar(ind, womenMeans, width, color='y', bottom=menMeans)
         #p3 = plt.bar(ind, dogMeans, width, color='b', bottom=humanMeans)
 
+    #sp.set_position([0.1,0.1,0.9,0.9])
     plt.ylabel('Scores')
     plt.title('Scores by group and gender')
     plt.xticks(ind+width/2., ('G1', 'G2', 'G3', 'G4', 'G5') )
     plt.yticks(np.arange(0,100,10))
     plt.legend(loc=1)
+    #sp.spines['left'].set_position(('data', 3.0))
+    #sp.spines['bottom'].set_position(('data', 40))
+    sp.spines['left'].set_position(('axes', 0.5))
+    sp.spines['bottom'].set_position(('axes', 0.5))
+    #sp.spines[direction].set_smart_bounds(True)
+    for direction in ["right","top"]:
+        sp.spines[direction].set_color('none')
     #plt.legend( (ret_values[0][0], ret_values[1][0], ret_values[2][0]), ('Men', 'Women', 'Dogs') )
 
-    plt.show()
+    fig.savefig(op_file, format='PDF', dpi=80, bbox_inches='tight')
+    op_file.close()
+    plt.close(fig)
+    #plt.show()
 
 N = 5
 menMeans = np.array([20, 35, 30, 35, 27])
